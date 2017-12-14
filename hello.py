948f22b645a257ccde979c12609791f7f843e54c
Lux <43660254@qq.com>
2017-12-14 13:51:51 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+print('hello python')
+
+name = input('please input your name: ')
+print('Hello,', name)
+
+num1 = input('please input first num1: ')
+num2 = input('please input first num2: ')
+print('the sum of num1 and num2 = ', num1+num2)
